ac7e9726ca3487bc14262e3df583b1bcc2d3f127
alu0100825145 <alu0100825145@ull.edu.es>
2016-10-18 19:03:04 +0100

    Update script.js

--- Dia1/script.js
+++ Dia1/script.js
@@ -12,3 +12,14 @@ $('.accordion').accordion({
   transitionSpeed: 400,		// velocidad del acordeón
   transitionEasing: 'ease'	// ritmo del acordeón (linear, ease-in…)
 });
+
+// Configuramos la librería Ouibounce
+ouibounce(document.getElementById('modal-card'), {
+  aggressive: true,			// borra la cookie viewedOuibounceModal
+  timer: 5						// espera (evita falsos positivos)
+});
+
+// Opcional (activa a los 5 segundos)
+var modal = ouibounce(...);
+setTimeout(function() { modal.fire() }, 5000);	// muestra a los 5seg
+
