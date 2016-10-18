bbd6296cc0574dea6e987aff4a0ba112cec636b7
alu0100825145 <alu0100825145@ull.edu.es>
2016-10-18 19:11:30 +0100

    Update script.js

--- Dia1/script.js
+++ Dia1/script.js
@@ -14,9 +14,11 @@ $('.accordion').accordion({
 });
 
 // Configuramos la librería Ouibounce
-ouibounce(document.getElementById('modal-card'), {
-  aggressive: true,			// borra la cookie viewedOuibounceModal
-  timer: 5						// espera (evita falsos positivos)
+$(document).ready(function() {
+  ouibounce(document.getElementById('modal-card'), {
+    aggressive: true,			// borra la cookie viewedOuibounceModal
+    timer: 5						// espera (evita falsos positivos)
+  });
 });
 
 
