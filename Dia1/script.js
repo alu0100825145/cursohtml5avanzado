1e5775c0199ebe0225c7b4b97b6079128a12687c
alu0100825145 <alu0100825145@ull.edu.es>
2016-10-18 18:13:02 +0100

    Update script.js

--- Dia1/script.js
+++ Dia1/script.js
@@ -6,3 +6,8 @@ $(document).ready(function() {
 $(document).ready(function() {
   $('.ba-slider').beforeAfter();
 });
+// Configuramos la librería jQuery Accordion
+$('.accordion').accordion({
+  transitionSpeed: 400,		// velocidad del acordeón
+  transitionEasing: 'ease'	// ritmo del acordeón (linear, ease-in…)
+});
