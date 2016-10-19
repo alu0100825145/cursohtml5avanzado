9bc6842638d3ff4f0150af24c7e4e1cf5c067cb2
alu0100825145 <alu0100825145@ull.edu.es>
2016-10-19 19:05:10 +0100

    Update script.js

--- Dia1/script.js
+++ Dia1/script.js
@@ -72,5 +72,30 @@ function viewTags() {
 	
 });
 
+$(document).ready(function() {
+	/* Javascript */
+	$('.editor').trumbowyg({
+	    svgPath: 'imagenes/icons.svg', // path of icons.svg
+	    // Habitualmente, se indica la ruta del SVG en este parámetro, pero
+	    // en codepen, simplemente pegaremos el contenido del SVG en la parte
+	    // del código HTML
+	});
+
+	$('#trumbowyg').trumbowyg('html'); // Obtienes el HTML generado
+	
+	$('.editor').trumbowyg({
+  lang: 'es', // Idioma (es.min.js incluido)
+  btns: [
+          ['bold', 'italic', 'underline', 'strikethrough'],
+          ['superscript', 'subscript'],
+          ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+          ['unorderedList', 'orderedList'],
+          ['link', 'insertImage'],
+          ['horizontalRule', 'removeformat'],
+          ['viewHTML'],
+          ['fullscreen']
+        ],
+});
+});
 
 
