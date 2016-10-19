ab0fac2df6c11743989d83242a4919ead62e22b5
alu0100825145 <alu0100825145@ull.edu.es>
2016-10-19 17:07:27 +0100

    Update script.js

--- Dia1/script.js
+++ Dia1/script.js
@@ -42,3 +42,8 @@ $(document).ready(function() {
   });
 });
 
+/* Javascript */
+var input = document.getElementById('datos');
+new Awesomplete(input, {
+	list: ["Ada", "Java", "JavaScript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"]
+});
