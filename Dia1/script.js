01492cad4102e2f8063ac9a8417892ec2f09fa6a
alu0100825145 <alu0100825145@ull.edu.es>
2016-10-20 19:27:05 +0100

    Update script.js

--- Dia1/script.js
+++ Dia1/script.js
@@ -159,3 +159,9 @@ $(document).ready(function() {
 	  speed: 500,				// Velocidad del giro
 	});
 });
+
+var PSV = PhotoSphereViewer({
+  panorama:  'imagenes/hermigua.jpg',
+  container: 'photosphere',
+  loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
+});
