9165c695c2bb771149137013436d657179f24c31
alu0100825145 <alu0100825145@ull.edu.es>
2016-10-20 16:50:18 +0100

    Update script.js

--- Dia1/script.js
+++ Dia1/script.js
@@ -135,6 +135,11 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 	var sample = 'A->B: Mensaje';
+	var sample1 = 'Title: Esto es un título';
+	var sample2 = 'B-->C: Línea a rayas';
+	var sample3 = 'C->>D: Línea con flecha hueca';
+	var sample4 = 'D-->>E: Línea a rayas con flecha hueca';
+	var sample5 = 'A->C: Uno \n B->C: Dos';
 
 	var diagram = Diagram.parse(sample);
 
