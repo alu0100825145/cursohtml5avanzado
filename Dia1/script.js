b3b75a2ee2d6b656cf7e82efa8d2501e9b4552c3
alu0100825145 <alu0100825145@ull.edu.es>
2016-10-20 16:54:28 +0100

    Update script.js

--- Dia1/script.js
+++ Dia1/script.js
@@ -134,14 +134,14 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-	var sample = 'A->B: Mensaje';
+	var sample = 'CD Tenerife->UD Las Palmas: Siempre gana a la';
 	var sample1 = 'Title: Esto es un título';
 	var sample2 = 'B-->C: Línea a rayas';
 	var sample3 = 'C->>D: Línea con flecha hueca';
 	var sample4 = 'D-->>E: Línea a rayas con flecha hueca';
 	var sample5 = 'A->C: Uno \n B->C: Dos';
 
-	var diagram = Diagram.parse([sample, sample1, sample2, sample3, sample4, sample5]);
+	var diagram = Diagram.parse(sample);
 
 	diagram.drawSVG('diagram', {
 		theme: 'hand'	// o 'simple'
