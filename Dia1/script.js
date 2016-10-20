bcf9f7bc7719f4996df405d91ea69d062d8edb62
alu0100825145 <alu0100825145@ull.edu.es>
2016-10-20 16:19:38 +0100

    Update script.js

--- Dia1/script.js
+++ Dia1/script.js
@@ -119,3 +119,16 @@ $(document).ready(function() {
 	  }
 	});
 });
+$(document).ready(function() {
+	var data = {
+	  labels: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
+	  series: [{ data: [2, 4, 6, 8, 10, 12] }, // ct-series-a
+		   { data: [1, 2, 3, 4, 5, 6] },   // ct-series-b
+		   { data: [12, 10, 8, 6, 4, 2] }  // ct-series-c
+		  ]
+	};
+
+	var options = {}
+
+	new Chartist.Line('.chart', data, options);
+});
