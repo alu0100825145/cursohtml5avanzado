4ccab6257533b9b0375d78ef8afe703a641d7f1b
alu0100825145 <alu0100825145@ull.edu.es>
2016-10-28 17:04:44 +0100

    Update script.js

--- Dia1/script.js
+++ Dia1/script.js
@@ -169,3 +169,18 @@ $(document).ready(function() {
 $(document).ready(function() {
 	impress().init();
 });
+$(document).ready(function() {
+	var hora = document.getElementById('horas');
+	var minuto = document.getElementById('minutos');
+	var segundo = document.getElementById('segundos');
+
+	function actualiza(){
+	  var hours = new Date().getHours();
+	  var minutes = new Date().getMinutes();
+	  var seconds = new Date().getSeconds();
+	  hora.textContent=hours;
+	  minuto.textContent=minutes;
+	  segundo.textContent=seconds;
+	}
+ 	var intervalo = setInterval(actualiza, 1000);
+});
